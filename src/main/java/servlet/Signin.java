8158404e34e3c3ef32e590ffcd6b4c2e74621df0
zambospam <zambospam@gmail.com>
2020-06-30 11:54:40 +0200

    LocalDate parsing

--- src/main/java/servlet/Signin.java
+++ src/main/java/servlet/Signin.java
@@ -1,6 +1,10 @@
 package servlet;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,6 +26,8 @@ public class Signin extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String dateString = request.getParameter("date2");
+		LocalDate birtDate = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 //		User user = new User(request.getParameter("inputname"), request.getParameter("inputlastname"), request.getParameter("date2"), request.getParameter("inputEmail4"), request.getParameter("inputnumber"), request.getParameter("inputusername"), request.getParameter("inputPassword4"), request.getParameter("inputCity"), request.getParameter("inputAddress"), request.getParameter("inputZip"));
 //		UserDao userDao = new UserDao();
 //		userDao.save(user);
