0b1a4eab67c40de750154a6ae1f9790dedcd1cc8
Davide <d.depasquale94@gmail.com>
2020-06-30 12:29:33 +0200

    Fixed date type in Signin.java + removed doGet method

--- src/main/java/servlet/Signin.java
+++ src/main/java/servlet/Signin.java
@@ -21,13 +21,9 @@ import dao.UserDao;
 public class Signin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String dateString = request.getParameter("date2");
-		LocalDate birthDate = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		LocalDate birthDate = LocalDate.parse(dateString, DateTimeFormatter.ISO_LOCAL_DATE);
 		User user = new User(request.getParameter("inputname"), request.getParameter("inputlastname"), birthDate, request.getParameter("inputEmail4"), request.getParameter("inputnumber"), request.getParameter("inputusername"), request.getParameter("inputPassword4"), request.getParameter("inputCity"), request.getParameter("inputAddress"), Integer.parseInt(request.getParameter("inputZip")));
 		UserDao userDao = new UserDao();
 		userDao.save(user);
