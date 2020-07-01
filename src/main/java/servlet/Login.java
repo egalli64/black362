3f66ad71c87bc3b3c4264a4f6323f085bc1653fd
Davide <d.depasquale94@gmail.com>
2020-07-01 14:42:12 +0200

    Updated Login.java and loginResult.jsp

--- src/main/java/servlet/Login.java
+++ src/main/java/servlet/Login.java
@@ -2,12 +2,18 @@ package servlet;
 
 import java.io.IOException;
 
+
+import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import dao.User;
+import dao.UserDao;
 
 /**
  * Servlet implementation class Login
@@ -15,11 +21,31 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	@Resource(name = "jdbc/black362")
+    private DataSource ds;
        
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//username e password
 		
-		RequestDispatcher rd = request.getRequestDispatcher("/.jsp");
-        rd.forward(request, response);
+		try (UserDao userDao = new UserDao(ds)) {
+			String username = request.getParameter("username");
+			String password = request.getParameter("password");
+			
+			if (userDao.usernameExists(username)) {
+				if (userDao.passwordCheck(username, password)) {
+					User user = userDao.getByUsername(username);
+					request.setAttribute("message", "Bentornato " + username);
+				} else {
+					request.setAttribute("message", "La password inserita non è corretta");
+				}
+			} else {
+				request.setAttribute("message", "Utente non trovato.");
+			}
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/loginResult.jsp");
+	        rd.forward(request, response);
+		}
 	}
 
 }
