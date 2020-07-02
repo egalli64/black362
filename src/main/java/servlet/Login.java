63da877f05f42c695bb762868f363eaa647bbd79
zambospam <zambospam@gmail.com>
2020-07-02 11:51:50 +0200

    login servlet minor changes

--- src/main/java/servlet/Login.java
+++ src/main/java/servlet/Login.java
@@ -1,7 +1,7 @@
 package servlet;
 
 import java.io.IOException;
-
+import java.io.PrintWriter;
 
 import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
@@ -31,6 +31,8 @@ public class Login extends HttpServlet {
 			String username = request.getParameter("username");
 			String password = request.getParameter("password");
 			
+			RequestDispatcher rd = null;
+			
 			if (userDao.usernameExists(username)) {
 				if (userDao.passwordCheck(username, password)) {
 					User user = userDao.getByUsername(username);
@@ -38,16 +40,15 @@ public class Login extends HttpServlet {
 					HttpSession session = request.getSession(true);
 					session.setAttribute("user", username);
 					
-					request.setAttribute("message", "Bentornato " + username);
+					rd = request.getRequestDispatcher("/ristorante.jsp");
 				} else {
-					request.setAttribute("message", "La password inserita non è corretta");
+					rd = request.getRequestDispatcher("/Login.jsp");
 				}
 			} else {
-				request.setAttribute("message", "Utente non trovato.");
+				rd = request.getRequestDispatcher("/Login.jsp");
 			}
 			
-			RequestDispatcher rd = request.getRequestDispatcher("/loginResult.jsp");
-	        rd.forward(request, response);
+			rd.forward(request, response);
 		}
 	}
 	
